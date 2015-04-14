9db90ec0425d2c26a06d28f9c3dcd12f753bb116
Kevin Shiuan <shiuan.kevin@gmail.com>
2015-04-13 22:00:04 -0400

    fixed_errors

--- guess_fking_numbers.py
+++ guess_fking_numbers.py
@@ -1,20 +1,17 @@
 import random
 
 def start_game(z):
-  print("Random number has been fucking picked!")
+  print("Random number has been fucking picked! You have 5 stupid guesses...na")
   return(random.randint(1,z))
 
 def guess():
   while True:
-  	x = input("What's your fucking guess between 1 and 10?: ")
-  	
   	try:
-  		y = int(x)
+  		y = int(input("What's your fucking guess between 1 and 10?: "))
   		break
   	except:
   		print('That\'s not a fucking whole number! Try again...')
-  		pass
-  		
+  		pass		
   return(y)
   
 def check(x,y):
@@ -36,17 +33,18 @@ def play_again():
     return(0)
 
 high_num = 10
+count = 5
 
 while True:
   start = start_game(high_num)
-  count = 5
   guesses = []
   while count - len(guesses) > 0:
     user_guess = guess()
     if check(start,user_guess) == 1:
       break
-    print("You've got {} more guesses, you pig.".format(str(count)))
-    user_guess.app(guesses)
+    guesses.append(user_guess)
+    print("You've got {} more guesses, you pig. You previously fucking guessed {}".
+    	format(str(count - len(guesses)),str(', '.join(str(e) for e in guesses))))
       
   print("Game over, LOSER! Play again, LOSER? Yes or No?")
   
