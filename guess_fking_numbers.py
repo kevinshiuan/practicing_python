0ce455f61204d09c75d06531c2482364c6286f16
Kevin Shiuan <shiuan.kevin@gmail.com>
2015-04-13 22:13:50 -0400

    final

--- guess_fking_numbers.py
+++ guess_fking_numbers.py
@@ -1,7 +1,7 @@
 import random
 
 def start_game(z):
-  print("Random number has been fucking picked! You have 5 stupid guesses...na")
+  print("Random number has been fucking picked! You have 5 stupid guesses...")
   return(random.randint(1,z))
 
 def guess():
@@ -24,13 +24,6 @@ def check(x,y):
   elif y > x:
     print("Too high, you pothead!")
     return(2)
-  
-def play_again():
-  playy = input("> ")
-  if playy == 'Yes':
-    return(1)
-  elif playy == 'No':
-    return(0)
 
 high_num = 10
 count = 5
@@ -45,8 +38,8 @@ while True:
     guesses.append(user_guess)
     print("You've got {} more guesses, you pig. You previously fucking guessed {}".
     	format(str(count - len(guesses)),str(', '.join(str(e) for e in guesses))))
-      
-  print("Game over, LOSER! Play again, LOSER? Yes or No?")
+
+  y = input("Game over, LOSER! Play again, LOSER? 1 for yes, 0 for no -_-: ")
   
-  if play_again() == 0:
+  if y == 0:
     break
